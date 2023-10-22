000fa95cb0d03d610f81d7f8af6f04fb8a2adad7
Samuel Leclerc <samuellaurentleclerc@gmail.com>
2023-10-21 22:23:00 -0400

    Add raycast sensors

---
+++ sensor.js
@@ -0,0 +1,99 @@
+class Sensor {
+  constructor(car) {
+    this.car = car;
+    this.rayCount = 5;
+    this.rayLength = 150;
+    this.raySpread = Math.PI / 2;
+
+    this.rays = [];
+    this.readings = [];
+  }
+
+  update(roadBorders) {
+    this.#castRays();
+    this.readings = [];
+    for (let i = 0; i < this.rays.length; i++) {
+      this.readings.push(this.#getReading(this.rays[i], roadBorders));
+    }
+  }
+
+  #getReading(ray, roadBorders) {
+    let touches = [];
+    for (let i = 0; i < roadBorders.length; i++) {
+      const touch = getIntersection(
+        ray[0],
+        ray[1],
+        roadBorders[i][0],
+        roadBorders[i][1]
+      );
+      if (touch) {
+        touches.push(touch);
+      }
+    }
+
+    if (!touches.length) {
+      return null;
+    } else {
+      let minOffset = Infinity;
+      let minIndex = null;
+
+      for (let i = 0; i < touches.length; i++) {
+        if (touches[i].offset < minOffset) {
+          minOffset = touches[i].offset;
+          minIndex = i;
+        }
+      }
+
+      return touches[minIndex];
+      // const offsets = touches.map((touch) => touch.offset);
+      // const minOffset = Math.min(...offsets);
+      // return touch.find((touch) => touch.offset === minOffset);
+    }
+  }
+
+  #castRays() {
+    this.rays = [];
+
+    for (let i = 0; i < this.rayCount; i++) {
+      const rayAngle =
+        linearInterpolation(
+          this.raySpread / 2,
+          -this.raySpread / 2,
+          this.rayCount === 1 ? 0.5 : i / (this.rayCount - 1)
+        ) + this.car.angle;
+
+      const start = {
+        x: this.car.x,
+        y: this.car.y,
+      };
+      const end = {
+        x: this.car.x - Math.sin(rayAngle) * this.rayLength,
+        y: this.car.y - Math.cos(rayAngle) * this.rayLength,
+      };
+
+      this.rays.push([start, end]);
+    }
+  }
+
+  draw(context) {
+    for (let i = 0; i < this.rayCount; i++) {
+      let end = this.rays[i][1];
+      if (this.readings[i]) {
+        end = this.readings[i];
+      }
+      context.beginPath();
+      context.lineWidth = 2;
+      context.strokeStyle = "yellow";
+      context.moveTo(this.rays[i][0].x, this.rays[i][0].y);
+      context.lineTo(end.x, end.y);
+      context.stroke();
+
+      context.beginPath();
+      context.lineWidth = 2;
+      context.strokeStyle = "black";
+      context.moveTo(this.rays[i][1].x, this.rays[i][1].y);
+      context.lineTo(end.x, end.y);
+      context.stroke();
+    }
+  }
+}
