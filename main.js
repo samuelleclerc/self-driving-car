9579eca080081d8226a3e63583b8fa3b154f9caa
Samuel Leclerc <samuellaurentleclerc@gmail.com>
2023-10-23 21:23:49 -0400

    Add traffic

--- main.js
+++ main.js
@@ -3,19 +3,27 @@ canvas.width = 200;
 
 const context = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
 
 animate();
 
 function animate() {
-  car.update(road.borders);
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].update(road.borders, []);
+  }
+  car.update(road.borders, traffic);
+
   canvas.height = window.innerHeight;
 
   context.save();
   context.translate(0, -car.y + canvas.height * 0.7);
 
   road.draw(context);
-  car.draw(context);
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].draw(context, "red");
+  }
+  car.draw(context, "blue");
 
   context.restore();
   requestAnimationFrame(animate);
